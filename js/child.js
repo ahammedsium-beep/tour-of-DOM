a1321b268fc993a07fae0b7d5a112da1051b5d92
ahammedSium <siumshad16@gmail.com>
2026-02-19 23:51:50 +0600

    22-7

---
+++ js/child.js
@@ -0,0 +1,24 @@
+// document.getElementById('players-container').childNodes[3].childNodes[1].parentNode.parentNode.parentNode
+
+// create element and set innerText or innerHTML
+const newChild = document.createElement('li')
+newChild.innerText = 'New born footballer here'
+
+// find the parent where you will add the child
+const playerList = document.getElementById('player-list')
+
+// append the child to the parent
+playerList.appendChild(newChild)
+
+// old dhaka id te ekta h1 tag add korbo
+const newH1 = document.createElement('h1')
+newH1.innerText = 'Practice HTML tags adding'
+
+const oldDhaka = document.getElementById('old-dhaka')
+oldDhaka.appendChild(newH1)
+
+newH1.style.color = 'gray'
+newH1.style.background = 'pink'
+newH1.style.padding = '15px'
+newH1.style.borderRadius = '13px'
+newH1.style.textAlign = 'center'
